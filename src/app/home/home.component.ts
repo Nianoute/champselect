8908c14b52e4f4bd337ac57cb75baaa350fb01af
nianoute <enzo.angot@gmail.com>
2024-03-22 17:49:43 +0100

    refactor: home v1, button create v1, change background

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  intervalId: number | undefined = 0;
   gamename: string = localStorage.getItem('gamename') || '';
   bluename: string = localStorage.getItem('bluename') || '';
   redname: string = localStorage.getItem('redname') || '';
   error: boolean = false;
+
+  background: string = 'url(../../assets/img/background.jpg)';
+  backgroundElement: HTMLElement | null = null;
+  backgrounds = [
+    'url(../../assets/img/background__teemo.png)',
+    'url(../../assets/img/fiddle.gif)',
+    'url(https://media.giphy.com/media/2DfPsGDCCFR0VO2yhx/giphy.gif)',
+    'url(https://media.giphy.com/media/ip68eDWiINMQh3qysS/giphy.gif)',
+    'url(https://media.giphy.com/media/Mk8X5m74dCTJVMLoQH/giphy.gif)',
+    'url(https://media.giphy.com/media/ZztwJB3iqzY1kdJrQ4/giphy.gif)',
+    'url(https://media.giphy.com/media/ce7o3JR18Ryr8yGN60/giphy.gif)',
+    'url(https://media.giphy.com/media/ZRR8wnQpPet9p5K5rX/giphy.gif)',
+    'url(../../assets/img/background.jpg)',
+  ];
+  currentBackground: number = 0;
 
   async createGame() {
     this.error = false;
@@ -25,12 +41,31 @@ export class HomeComponent {
     }
 
     if (!this.error) {
-      console.log('Game name:', this.gamename);
-      console.log('Blue team:', this.bluename);
-      console.log('Red team:', this.redname);
       localStorage.setItem('bluename', this.bluename);
       localStorage.setItem('redname', this.redname);
       localStorage.setItem('gamename', this.gamename);
+    }
+  }
+
+  async reset() {
+    this.gamename = '';
+    this.bluename = '';
+    this.redname = '';
+    localStorage.removeItem('gamename');
+    localStorage.removeItem('bluename');
+    localStorage.removeItem('redname');
+  }
+
+  async backgroundChange() {
+    this.backgroundElement =
+      document.documentElement.querySelector('.background');
+    if (this.backgroundElement) {
+      this.backgroundElement.style.backgroundImage =
+        this.backgrounds[this.currentBackground];
+      this.currentBackground++;
+      if (this.currentBackground === this.backgrounds.length) {
+        this.currentBackground = 0;
+      }
     }
   }
 }
